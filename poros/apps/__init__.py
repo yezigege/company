9cfd61cd08323a027ba06b4ecfe8b865aeee6a38
yezigege <1757423278@qq.com>
2022-04-12 17:13:50 +0800

    add: 新增警告规则过滤器

--- poros/apps/__init__.py
+++ poros/apps/__init__.py
@@ -13,6 +13,7 @@ def create_app(config_filename: str = 'conf.settings'):
     app.config.from_object(config_filename)
     
     configure_logger(app)
+    filter_warning()
     return app
 
 
@@ -20,3 +21,14 @@ def configure_logger(app):
     """Configure loggers. 配置日志打印等级为设定等级以上才打印"""
     rootLogger = logging.getLogger()
     rootLogger.setLevel(app.config.get('LOG_LEVEL') or 'WARN')
+    
+def filter_warning():
+    """警告过滤器
+    通过设置 message 来正则匹配 warning 消息，如果匹配到。就忽略该警告。
+    https://blog.csdn.net/TeFuirnever/article/details/94122670
+    """
+    import warnings
+    warnings.filterwarnings(
+        "ignore",
+        message="Multiple schemas resolved to the name ",
+    )
