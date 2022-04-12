16325f307e786142ce75b83653e6d492ce39f92c
yezigege <1757423278@qq.com>
2022-04-12 17:05:36 +0800

    add: 新增日志记录级别设置

--- poros/apps/__init__.py
+++ poros/apps/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import logging
 
 
 def create_app(config_filename: str = 'conf.settings'):
@@ -11,4 +12,11 @@ def create_app(config_filename: str = 'conf.settings'):
     app = Flask(__name__)
     app.config.from_object(config_filename)
     
+    configure_logger(app)
     return app
+
+
+def configure_logger(app):
+    """Configure loggers. 配置日志打印等级为设定等级以上才打印"""
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(app.config.get('LOG_LEVEL') or 'WARN')
--- poros/conf/settings.py
+++ poros/conf/settings.py
@@ -1,4 +1,8 @@
 from distutils.debug import DEBUG
+from distutils.log import INFO
 
 
 DEBUG=True
+
+# 记录日志级别
+LOG_LEVEL='INFO'
